8a8f645ae473795872d43d621c96b5ee2abdb0e0
Marco Bernasocchi <marco@opengis.ch>
2017-10-16 02:47:55 +0200

    added 2 scales

--- static/js/chart.js
+++ static/js/chart.js
@@ -70,6 +70,30 @@ var myConfig = {
         },
         item: {
             fontColor: '#E3E3E5'
+        },
+        format:"%v m3/s"
+    },
+    "scale-y-2": {
+        minorTicks: 1,
+        lineColor: '#E3E3E5',
+        tick: {
+            lineColor: '#E3E3E5'
+        },
+        minorTick: {
+            lineColor: '#E3E3E5'
+        },
+        minorGuide: {
+            visible: true,
+            lineWidth: 1,
+            lineColor: '#E3E3E5',
+            alpha: 0.7,
+            lineStyle: 'dashed'
+        },
+        guide: {
+            lineStyle: 'dashed'
+        },
+        item: {
+            fontColor: '#E3E3E5'
         }
     },
     tooltip: {
@@ -128,19 +152,21 @@ var myConfig = {
         }
     }],
     series: [{
-        text: "Deflusso m3/s",
+        text: "Deflusso",
         values: oasi_values,
+        scales: "scale-x, scale-y",
         lineColor: '#E34247',
         marker: {
             backgroundColor: '#E34247'
         }
-    }, /*{
+    }, {
         values: [165.57, 170.47, 197.17, 164.64, 132.73, 176.89, 139.41, 158.71, 177.85, 138.87, 135.74, 167.06, 156.42, 182, 169.73, 151.08, 165.58, 146.29, 124.5, 181.71, 143.96, null, null, null, 146, 172.6, 149.81, 161.09, 175.88, 149.39, 184.1, 123.85, 186.82, 139.72, 138.61, 170.28, 164.06, 184.33, null, null, 131.85, 133.32, 134.49, 143.79, 125.23],
+        scales: "scale-x, scale-y-2",
         lineColor: '#FEB32E',
         marker: {
             backgroundColor: '#FEB32E'
         }
-    }, {
+    }, /*{
         values: [70.19, 96.56, 75.04, 51.58, 82.8, 68.14, 89.61, null, 45.33, 98.59, 92.9, 66.94, null, 74.08, 57.25, 79.68, 89.66, 64.56, 96.59, 79.96, 98.08, 42.93, 91.93, 56.23, 82.66, null, 85.76, 74.98, 51, 66.99, 63.02, 63.8, 44.33, 77.56, 95.28, 60.93, 59.6, 80.58, 68.51, 67.63, 69.76, 40.54, 78.42, 90.4, 82.3],
         lineColor: '#31A59A',
         marker: {
